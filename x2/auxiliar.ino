1b26f03de0f4fc00d4f1d9920e195edd772b9a8c
Márcio Pessoa <marcio.pessoa@gmail.com>
2023-09-05 06:10:33 -0300

    Format file

--- x2/auxiliar.ino
+++ x2/auxiliar.ino
@@ -1,19 +1,19 @@
 /* auxiliar.ino, x2 Mark I - Two Axes Platform, Arduino auxiliar sketch file
- * 
+ *
  * Author: Márcio Pessoa <marcio.pessoa@gmail.com>
  * Contributors: none
  */
 
 /* isAllDone
- * 
+ *
  * Description
  *   Checks if all axes is done (not moving).
- * 
+ *
  *   isAllDone()
- * 
+ *
  * Parameters
  *   none
- * 
+ *
  * Returns
  *   bool: 0 - No axis moving.
  *         1 - There are on or more axes moving.
@@ -22,22 +22,21 @@ bool isAllDone() {
   if (x_axis.isDone() and
       y_axis.isDone()) {
     return true;
-  }
-  else {
+  } else {
     return false;
   }
 }
 
 /* spinCounter
- * 
+ *
  * Description
  *   Fan spin counter.
- * 
+ *
  *   spinCounter()
- * 
+ *
  * Parameters
  *   none
- * 
+ *
  * Returns
  *   void
  */
